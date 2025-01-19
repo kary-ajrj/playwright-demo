96598bf67d4e338b243c5653085939b202a8109c
Karishma <karishma@technogise.com>
2025-01-19 21:31:29 +0530

    Adds lint-staged and fixes code style.

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
-import playwright from 'eslint-plugin-playwright'
+import playwright from 'eslint-plugin-playwright';
 export default [
-    {
-        ...playwright.configs['flat/recommended'],
-        files: ['ui-tests/**.{js,ts}','api-tests/**.{js,ts}'],
-        rules: {
-            ...playwright.configs['flat/recommended'].rules,
-            "playwright/no-commented-out-tests":["warn"],
-            "playwright/no-duplicate-hooks":["warn"]
-        },
+  {
+    ...playwright.configs['flat/recommended'],
+    files: ['ui-tests/**.{js,ts}', 'api-tests/**.{js,ts}'],
+    rules: {
+      ...playwright.configs['flat/recommended'].rules,
+      'playwright/no-commented-out-tests': ['warn'],
+      'playwright/no-duplicate-hooks': ['warn'],
     },
-]
+  },
+];
